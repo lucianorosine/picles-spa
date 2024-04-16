37c6dfe4b80531a31c8a438f7fd9dde2af40aeaf
LucianoRosine <lucianorosine@gmail.com>
2024-04-16 19:06:51 -0300

    Criação dos Botoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Button } from "./components/common/Button/Button";
+import { ButtonVariant } from "./components/common/Button/Button.constants";
 
-export function App() {
+export function App(){
   const [count, setCount] = useState(0)
 
-  return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
+  return( 
+    <>
+    <Button variant = {ButtonVariant.Default}onClick={() => setCount(count +1)}> Quero Adotar</Button>
+    <Button variant = {ButtonVariant.Outlined}onClick={() => setCount(count +1)}> Tenho um abrigo</Button>
+    </>
   )
 }
